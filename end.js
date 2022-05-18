dc677a402724bbc3a21231aef56c53049fe9e87c
Grant Royseth <groyseth@gmail.com>
2022-05-18 16:26:03 -0500

    commit

--- end.js
+++ end.js
@@ -2,8 +2,8 @@ console.log("working");
 var input = localStorage.getItem("userChoice")
 console.log(input);
 
-function submitFunction(){
-    console.log("click");
-    location.replace('./frontPage.html')
-  } 
+// function submitFunction(){
+//     console.log("click");
+//     location.replace('./frontPage.html')
+//   } 
 document.getElementById('userInput').innerHTML = `You selected ${input} out of 5`
