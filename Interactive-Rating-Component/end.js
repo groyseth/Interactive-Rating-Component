208f34beb69f4043237590743bb8f155881f7e97
Grant Royseth <groyseth@gmail.com>
2022-05-18 16:21:41 -0500

    file move

--- Interactive-Rating-Component/end.js
+++
@@ -1,9 +0,0 @@
-console.log("working");
-var input = localStorage.getItem("userChoice")
-console.log(input);
-
-function submitFunction(){
-    console.log("click");
-    location.replace('./frontPage.html')
-  } 
-document.getElementById('userInput').innerHTML = `You selected ${input} out of 5`
